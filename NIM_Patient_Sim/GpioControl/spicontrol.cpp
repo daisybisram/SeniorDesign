b6fce225cdbb70387b1616f25357fd8a9b55d6f7
daisybisram <59884330+daisybisram@users.noreply.github.com>
2020-01-22 10:40:28 -0500

    add adc registers

--- NIM_Patient_Sim/GpioControl/spicontrol.cpp
+++ NIM_Patient_Sim/GpioControl/spicontrol.cpp
@@ -1,16 +1,51 @@
 #include "spicontrol.h"
 #include "ui_spicontrol.h"
 #include "stdint.h"
+#include <errno.h>
+#include <libgen.h>
+#include <limits.h>
+#include <signal.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+#include <libdigiapix/spi.h>
+
+#define DEFAULT_SPI_ALIAS		"DEFAULT_SPI"
+#define DEFAULT_SPI_ADDRESS_SIZE	1
+#define DEFAULT_SPI_PAGE_SIZE		16
+#define DEFAULT_SPI_PAGE_INDEX		0
+
+#define ARG_SPI_DEVICE		0
+#define ARG_SPI_SLAVE		1
+
+#define BUFF_SIZE			256
+
+#define WREN				0x06
+#define WRDI				0x04
+#define WRITE				0x02
+#define READ				0x03
+#define RDSR				0x05
+
+#define CLK_MODE			SPI_CLK_MODE_0
+#define CHIP_SELECT			SPI_CS_ACTIVE_LOW
+#define BIT_ORDER			SPI_BO_MSB_FIRST
+#define MAX_BUS_SPEED		1000000 /* 1MHz */
+#define BITS_PER_WORD		SPI_BPW_8
+
+#define OPERATION_BYTES		1
+
+static spi_t *spi_dev;
+static unsigned int page_size, address_bytes = 0;
+static uint8_t *tx_buffer;
+static uint8_t *rx_buffer;
 
 SpiControl::SpiControl(QWidget *parent) :
     QScrollArea(parent),
     ui(new Ui::SpiControl)
 {
-    static spi_t *spi_dev;
-    static unsigned int page_size, address_bytes = 0;
-    static uint8_t *tx_buffer;
-    static uint8_t *rx_buffer;
-
     int spi_device = 0, spi_slave = 0, page_index = 0, i = 0;
     spi_transfer_cfg_t transfer_mode = {0};
     char *name = basename(argv[0]);
@@ -27,25 +62,29 @@ SpiControl::SpiControl(QWidget *parent) :
     transfer_mode.clk_mode = CLK_MODE;
     transfer_mode.chip_select = CHIP_SELECT;
     transfer_mode.bit_order = BIT_ORDER;
-    if (ldx_spi_set_transfer_mode(spi_dev, &transfer_mode) != EXIT_SUCCESS) {
+    if (ldx_spi_set_transfer_mode(spi_dev, &transfer_mode) != EXIT_SUCCESS)
+    {
         printf("Failed to configure SPI transfer mode\n");
         return EXIT_FAILURE;
     }
 
     /* Configure the bits-per-word */
-    if (ldx_spi_set_bits_per_word(spi_dev, BITS_PER_WORD) != EXIT_SUCCESS) {
+    if (ldx_spi_set_bits_per_word(spi_dev, BITS_PER_WORD) != EXIT_SUCCESS)
+    {
         printf("Failed to configure SPI bits-per-word\n");
         return EXIT_FAILURE;
     }
 
     /* Configure the max bus speed */
-    if (ldx_spi_set_speed(spi_dev, MAX_BUS_SPEED) != EXIT_SUCCESS) {
+    if (ldx_spi_set_speed(spi_dev, MAX_BUS_SPEED) != EXIT_SUCCESS)
+    {
         printf("Failed to configure SPI max bus speed\n");
         return EXIT_FAILURE;
     }
 
     /* Set the write-enable bit. */
-    if (spi_Write_enable() != EXIT_SUCCESS) {
+    if (spi_Write_enable() != EXIT_SUCCESS)
+    {
         printf("Failed to set the write-enable bit\n");
         return EXIT_FAILURE;
     }
@@ -53,7 +92,8 @@ SpiControl::SpiControl(QWidget *parent) :
     /* Initialize the write and read buffers */
     tx_buffer = (uint8_t *)calloc(page_size, sizeof(uint8_t));
     rx_buffer = (uint8_t *)calloc(page_size, sizeof(uint8_t));
-    if (tx_buffer == NULL || rx_buffer == NULL) {
+    if (tx_buffer == NULL || rx_buffer == NULL)
+    {
         printf("Failed to initialize read/write buffers\n");
         return EXIT_FAILURE;
     }
@@ -62,21 +102,21 @@ SpiControl::SpiControl(QWidget *parent) :
     srand(time(NULL));
     for (i = 0; i < (page_size); i++) {
         tx_buffer[i] = rand() % 255;
-    }*/
+    }
 
-    /* Write the page. */
+     Write the page.
     if (spi_Write(page_index, tx_buffer) != EXIT_SUCCESS) {
         printf("Write page failed\n");
         return EXIT_FAILURE;
     }
 
-    /* Read the page. */
+     Read the page.
     if (spi_Read(page_index, rx_buffer) != EXIT_SUCCESS) {
         printf("Read page failed\n");
         return EXIT_FAILURE;
     }
 
-    /* Validate the read data. */
+    Validate the read data.
     printf("[INFO] Validating read data...\n");
     for (i = 0; i < page_size; i++) {
         printf("  Byte %d: Write 0x%02x - Read 0x%02x", i,
@@ -86,7 +126,7 @@ SpiControl::SpiControl(QWidget *parent) :
         } else {
             printf(" - Incorrect\n");
         }
-    }
+    }*/
 
     /* 'atexit' executes the cleanup function */
     return EXIT_SUCCESS;
